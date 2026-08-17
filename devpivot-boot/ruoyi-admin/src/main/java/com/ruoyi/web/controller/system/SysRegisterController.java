149f7d748cc063d05f3a6f5e08d29cd9198a4b05
zhangqibo <3368776560@qq.com>
2026-08-17 14:35:52 +0800

    feat: 优化注册控制器，简化请求映射并添加注册状态查询接口

--- devpivot-boot/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
+++ devpivot-boot/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
@@ -1,9 +1,8 @@
 package com.ruoyi.web.controller.system;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.ruoyi.common.annotation.Anonymous;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.domain.model.RegisterBody;
@@ -17,6 +16,7 @@ import com.ruoyi.system.service.ISysConfigService;
  * @author ruoyi
  */
 @RestController
+@RequestMapping("/register")
 public class SysRegisterController extends BaseController
 {
     @Autowired
@@ -25,7 +25,7 @@ public class SysRegisterController extends BaseController
     @Autowired
     private ISysConfigService configService;
 
-    @PostMapping("/register")
+    @PostMapping
     public AjaxResult register(@RequestBody RegisterBody user)
     {
         if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
@@ -34,5 +34,21 @@ public class SysRegisterController extends BaseController
         }
         String msg = registerService.register(user);
         return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+
+    /**
+     * 查询是否开放注册（免登录，供登录页据此显隐注册入口）
+     * 由后台参数 sys.account.registerUser 控制，默认 false
+     *
+     * 注意：类级 @RequestMapping("/register") + 本方法 /enabled = 实际路径 /register/enabled，
+     * 与前端 api/login.js 的 getRegisterEnabled() 保持一致；
+     * 此前无类级前缀导致实际路径是 /enabled，前端请求 /register/enabled 落空，
+     * 被 Spring Security 判为未认证返回 401，触发全局「重新登录」弹窗死循环。
+     */
+    @Anonymous
+    @GetMapping("/enabled")
+    public AjaxResult registerEnabled()
+    {
+        return success("true".equals(configService.selectConfigByKey("sys.account.registerUser")));
     }
 }
